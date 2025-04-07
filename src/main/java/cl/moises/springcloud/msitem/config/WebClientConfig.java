d3acacafd486b33e11cd28c54461f45217233139
MoisesCornejo <moises.cornejo@inacapmail.cl>
2025-04-07 02:09:39 -0400

    Actualizar configuración de WebClient para usar URL base desde properties

--- src/main/java/cl/moises/springcloud/msitem/config/WebClientConfig.java
+++ src/main/java/cl/moises/springcloud/msitem/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package cl.moises.springcloud.msitem.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +9,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${config.base.url.ms-productos}")
+    private String url;
+
     @Bean
     @LoadBalanced
     WebClient.Builder webClient() {
-        return WebClient.builder().baseUrl("http://ms-productos/api/v1");
+        return WebClient.builder().baseUrl(url);
     }
 
 }
