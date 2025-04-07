0f81f39b9be525f18fc3009c90a5e40067a5d891
MoisesCornejo <moises.cornejo@inacapmail.cl>
2025-04-07 00:27:55 -0400

    Agregar controlador ItemController para gestionar operaciones de ítems

---
+++ src/main/java/cl/moises/springcloud/msitem/controller/ItemController.java
@@ -0,0 +1,38 @@
+package cl.moises.springcloud.msitem.controller;
+
+import cl.moises.springcloud.msitem.models.Item;
+import cl.moises.springcloud.msitem.services.ItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/items")
+    public ResponseEntity<List<Item>> findAll() {
+        return ResponseEntity.ok().body(itemService.findAll());
+    }
+
+    @GetMapping("/item/{id}")
+    public ResponseEntity<Item> findById(@PathVariable Long id) {
+        Optional<Item> itemOptional = itemService.findById(id);
+        if (itemOptional.isPresent()) {
+            return ResponseEntity.ok().body(itemOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
