407b79c4394583efe158ef01e18195c46ae61815
MoisesCornejo <moises.cornejo@inacapmail.cl>
2025-04-06 23:35:47 -0400

    Habilitar clientes Feign en la aplicación ms-item

--- src/main/java/cl/moises/springcloud/msitem/MsItemApplication.java
+++ src/main/java/cl/moises/springcloud/msitem/MsItemApplication.java
@@ -2,7 +2,9 @@ package cl.moises.springcloud.msitem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsItemApplication {
 
